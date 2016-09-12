b069c34722b254b541215dfbf4e73bf9bb2a3a3b
Sander Kruger <s.kruger@invessel.com>
2016-09-12 15:10:05 +0200

    Add prepareECC

--- clients/coinapult.php
+++ clients/coinapult.php
@@ -1,5 +1,8 @@
 <?php
 
+use Mdanter\Ecc\EccFactory;
+use Mdanter\Ecc\Crypto\Signature\Signer;
+use Mdanter\Ecc\Serializer\Signature\DerSignatureSerializer;
 
 class Coinapult
 {
@@ -8,32 +11,95 @@ class Coinapult
   /* Valid keys while searching for transactions. */
   private static $SEARCH_CRITERIA = array('transaction_id', 'type',
     'currency', 'to', 'from', 'extOID', 'txhash');
+  private static $COINAPULTPUB_PEM = "-----BEGIN PUBLIC KEY-----
+MFYwEAYHKoZIzj0CAQYFK4EEAAoDQgAEWp9wd4EuLhIZNaoUgZxQztSjrbqgTT0w
+LBq8RwigNE6nOOXFEoGCjGfekugjrHWHUi8ms7bcfrowpaJKqMfZXg==
+-----END PUBLIC KEY-----
+";
 
   private $_base_url = 'https://api.coinapult.com/api/';
 
   private $_api_key = null;
   private $_api_secret = null;
 
-  public function __construct($api_key, $api_secret, $base_url=NULL)
+  // ECC properties
+  private $ecc = false;
+  private $pubkey = null;
+  private $privkey = null;
+
+  public function __construct($ecc, $api_key, $api_secret, $base_url=NULL)
   {
-    $this->_api_key = $api_key;
-    $this->_api_secret = $api_secret;
+    $this->ecc = $ecc;
+    if ($ecc) {
+      $this->pubkey = $api_key;
+      $this->privkey = $api_secret;
+    } else {
+      $this->_api_key = $api_key;
+      $this->_api_secret = $api_secret;
+    }
     if (!is_null($base_url)) {
       $this->_base_url = $base_url;
     }
+  }
+
+  /* Auxiliary functions for sending signed requests to Coinapult. */
+  private function prepareECC($params) {
+    /* Define the headers and parameters required for sending
+     * a ECC signed request to Coinapult.
+     */
+    $headers = array();
+
+    if (isset($params['newAccount'])) {
+      /* Do not set a nonce when creating new account. */
+      $headers[] = 'cpt-ecc-new: ' . base64_encode($this->pubkey->pem);
+      unset($params['newAccount']);
+    } else {
+      $headers[] = 'cpt-ecc-pub: ' . $this->pubkey->hash;
+      $params['nonce'] = gen_nonce();
+    }
+    $params['timestamp'] = time();
+
+    $data = base64_encode(json_encode($params));
+    $adapter = EccFactory::getAdapter();
+    $generator = EccFactory::getNistCurves()->generator384();
+    $useDerandomizedSignatures = true;
+    $algorithm = 'sha256';
+
+    $signer = new Signer($adapter);
+    $hash = $signer->hashData($generator, $algorithm, $data);
+
+    if ($useDerandomizedSignatures) {
+      $random = \Mdanter\Ecc\Random\RandomGeneratorFactory::getHmacRandomGenerator($this->privkey->key, $hash, $algorithm);
+    } else {
+      $random = \Mdanter\Ecc\Random\RandomGeneratorFactory::getRandomGenerator();
+    }
+
+    $randomK = $random->generate($generator->getOrder());
+    $signature = $signer->sign($this->privkey->key, $hash, $randomK);
+    $serializer = new DerSignatureSerializer();
+    $serializedSig = $serializer->serialize($signature);
+    $headers[] = 'cpt-ecc-sign: ' . base64_encode($serializedSig);
+    
+    return array($headers, $data);
   }
 
   /* Make a call to the Coinapult API. */
   private function request($method, $params, $sign=TRUE, $post=TRUE) {
     $headers = array();
     if ($sign) {
-      $params['nonce'] = gen_nonce();
-      $params['timestamp'] = (string)time();
-      $params['endpoint'] = '/' . $method;
-      $headers[] = 'cpt-key: ' . $this->_api_key;
-      $signdata = base64_encode(json_encode($params));
-      $headers[] = 'cpt-hmac: ' . hash_hmac("sha512", $signdata, $this->_api_secret);
-      $data = array("data" => $signdata);
+      if ($this->ecc) {
+        $result = $this->prepareECC($params);
+        $headers = $result[0];
+        $data = array("data" => $result[1]);
+      } else {
+        $params['nonce'] = gen_nonce();
+        $params['timestamp'] = (string)time();
+        $params['endpoint'] = '/' . $method;
+        $headers[] = 'cpt-key: ' . $this->_api_key;
+        $signdata = base64_encode(json_encode($params));
+        $headers[] = 'cpt-hmac: ' . hash_hmac("sha512", $signdata, $this->_api_secret);
+        $data = array("data" => $signdata);
+      }
       $params_str = http_build_query($data, '', '&');
     } else {
       $params_str = http_build_query($params, '', '&');
@@ -184,6 +250,21 @@ class Coinapult
       }
     }
     return $res;
+  }
+
+  public function create_account() {
+    $params = array('newAccount' => true);
+    $data = $this->request('account/create', $params, true, true);
+    var_dump($data);
+    if ($data->error) {
+      return 'Error';
+    } else {
+      $rdata = $this->receiveECC($data);
+      if ($rdata->validSign && isset($rdata->success) && $rdata->success == $this->pubkey->hash) {
+        return 'Success';
+      }
+    }
+    return 'Error';
   }
 
 } /* Coinapult class. */
